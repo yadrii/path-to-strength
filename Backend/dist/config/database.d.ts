f2264a548a961b279601f15b3672663c1f52a56c
Prishika Chaudhary <prishikacdy10@gmail.com>
2026-03-29 00:44:39 +0530

    remove node_modules and dist from tracking

--- Backend/dist/config/database.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const connectDB: () => Promise<void>;
-export declare const disconnectDB: () => Promise<void>;
-//# sourceMappingURL=database.d.ts.map
--- Backend/dist/models/auth.d.ts
+++
@@ -1,7 +0,0 @@
-export interface authModel {
-    name: string;
-    email: string;
-    password: string;
-    role: 'user' | 'ngo';
-}
-//# sourceMappingURL=auth.d.ts.map
